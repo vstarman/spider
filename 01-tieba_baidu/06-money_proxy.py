0af6b90070524697f318cfc247f6d2f5f593c4ec
vstarman <vstarman@foxmail.com>
2017-12-17 16:26:45 +0800

    Custom opener to use the agent

--- 01-tieba_baidu/06-money_proxy.py
+++ 01-tieba_baidu/06-money_proxy.py
@@ -1,31 +1,25 @@
 # -*- coding:utf-8 -*-
-
 import urllib2
-
-# 构建了两个代理Handler，一个有代理IP，一个没有代理IP
-# http-->https才能打印
-http_proxy_handler = urllib2.ProxyHandler({"https": "124.88.67.81:80"})
-null_proxy_handler = urllib2.ProxyHandler({})
+import random
+from tools.users import MY_USER_AGENT
 
 
-# 定义一个代理开关
-proxy_switch = True
+def send_request():
+    # 1.create free proxy
+    # proxy = {}      # "origin": "211.103.136.242"
+    # proxy = {'http': '120.27.218.32:16816'}    # 未认证
+    # "origin": "120.27.218.32"
+    proxy = {'http': 'mr_mao_hacker:sffqry9r@120.27.218.32:16816'}
 
-# 通过 urllib2.build_opener()方法使用这些代理Handler对象，创建自定义opener对象
-# 根据代理开关是否打开，使用不同的代理模式
-if proxy_switch:
-    opener = urllib2.build_opener(http_proxy_handler)
-else:
-    opener = urllib2.build_opener(null_proxy_handler)
+    proxy_handler = urllib2.ProxyHandler(proxy)
+    opener = urllib2.build_opener(proxy_handler)
+    response = opener.open("http://www.httpbin.org/ip")
 
-request = urllib2.Request('http://www.baidu.com')
+    # install_opener将指定的openre对象修改为全局opener
+    urllib2.install_opener(opener)
 
-# 1. 如果这么写，只有使用opener.open()方法发送请求才使用自定义的代理，而urlopen()则不使用自定义代理。
-# response = opener.open(request)
+    return response.read()
 
-# 2. 如果这么写，就是将opener应用到全局，之后所有的，不管是opener.open()还是urlopen() 发送请求，都将使用自定义代理。
-urllib2.install_opener(opener)
-response = urllib2.urlopen(request)
 
-print response.read()
-
+if __name__ == '__main__':
+    print send_request()
