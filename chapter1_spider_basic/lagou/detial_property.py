40c2f9cb7c95b604ee1996e97f328219b0c21aa1
vstarman <vstarman@foxmail.com>
2017-12-21 19:25:12 +0800

    Change the method to a property

--- chapter1_spider_basic/lagou/detial_property.py
+++ chapter1_spider_basic/lagou/detial_property.py
@@ -9,9 +9,48 @@ class Person(object):
         return '%s %s' % (self.first_name, self.last_name)
 
 
+from decimal import Decimal
+
+
+########################################################################
+class Fees(object):
+    """"""
+
+    # ----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        self._fee = None
+
+    # ----------------------------------------------------------------------
+    @property
+    def fee(self):
+        """
+        Return the current fee
+        """
+        return self._fee
+
+    # ----------------------------------------------------------------------
+    @fee.setter
+    def fee(self, value):
+        """
+        Set the fee
+        """
+        if isinstance(value, str):
+            self._fee = Decimal(value)
+        elif isinstance(value, Decimal):
+            self._fee = value
+    # fee = property(get_fee, set_fee)
+
+
+
+
 if __name__ == '__main__':
-    person = Person('徐', '伟')
-    print person.first_name
-    print person.last_name
-    print person.full_name
+    f = Fees()
+    f.fee('809880')
+    print f.fee
+    # person = Person('徐', '伟')
+    # print person.first_name
+    # print person.last_name
+    # print person.full_name
+    # isinstance()
 
