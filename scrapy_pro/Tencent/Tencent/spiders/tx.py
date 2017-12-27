eeb9eba07c4b3dd5124e701d4863cce242265647
vstarman <vstarman@foxmail.com>
2017-12-27 19:32:48 +0800

    Use scrapy.Request send request

--- scrapy_pro/Tencent/Tencent/spiders/tx.py
+++ scrapy_pro/Tencent/Tencent/spiders/tx.py
@@ -8,12 +8,16 @@ class TxSpider(scrapy.Spider):
     name = 'tx'
     allowed_domains = ['hr.tencent.com']
     base_url = 'http://hr.tencent.com/position.php?keywords=&lid=0&start='
-    start_urls = [base_url + str(i) for i in range(0, 2681, 10)]
+    # start_urls = [base_url + str(i) for i in range(0, 2681, 10)]
+
+    # 1. 通过偏移量处理多个页面的规律情况
+    offset = 0
+    start_urls = [base_url + str(offset)]
 
     def parse(self, response):
         """制作item传给pipelines做最后处理"""
         node_list = response.xpath("//tr[@class='odd'] | //tr[@class='even']")
-        print '-' * 100
+        # print '-' * 100
         for node in node_list:
             item = PositionItem()
             item['position_link'] = node.xpath('./td/a/@href').extract_first()
@@ -22,6 +26,8 @@ class TxSpider(scrapy.Spider):
             item['position_nums'] = node.xpath('./td[3]/text()').extract_first()
             item['work_location'] = node.xpath('./td[4]/text()').extract_first()
             item['publish_time'] = node.xpath('./td[5]/text()').extract_first()
-            print '-' * 100
-            print item
             yield item
+
+        if self.offset < 2681:
+            self.offset += 10
+            yield scrapy.Request(url=self.base_url + str(self.offset), callback=self.parse)
